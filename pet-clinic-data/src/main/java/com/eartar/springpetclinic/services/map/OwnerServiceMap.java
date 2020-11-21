fbb8bd04338988d677a04f220a0eabf9023868fe
Emir <eartar@sabanciuniv.edu>
2020-11-21 14:14:45 +0300

    Impl map based services, closes #6.

---
+++ pet-clinic-data/src/main/java/com/eartar/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.eartar.springpetclinic.services.map;
+
+import com.eartar.springpetclinic.model.Owner;
+import com.eartar.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Owner findByID(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/eartar/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.eartar.springpetclinic.services.map;
+
+import com.eartar.springpetclinic.model.Pet;
+import com.eartar.springpetclinic.model.Pet;
+import com.eartar.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findByID(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
