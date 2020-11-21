c4c151114e6fbe7f504bacdd85c973ec63ba80b1
Emir <eartar@sabanciuniv.edu>
2020-11-21 15:04:09 +0300

    Added owner index page & controller, closes #11.

--- pet-clinic-web/src/main/java/com/eartar/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/eartar/springpetclinic/controllers/IndexController.java
@@ -3,10 +3,11 @@ package com.eartar.springpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/index")
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/", "index", "index.html"})
+    @RequestMapping({"", "/", ".html"})
     public String index(){
         return "index";
     }
