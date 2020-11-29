72cfe8bde57ba9a836d22a4bda578c65024d618c
Emir <eartar@sabanciuniv.edu>
2020-11-30 01:22:37 +0300

    Fixing request mappings

--- pet-clinic-web/src/main/java/com/eartar/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/eartar/springpetclinic/controllers/IndexController.java
@@ -3,13 +3,16 @@ package com.eartar.springpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/index")
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", ".html"})
+    @RequestMapping({"", "/", "index", "index.html"})
     public String index(){
         return "index";
     }
 
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notimplemented";
+    }
 }
