5ac47cdaec8da812c94a20148854fcc6b1c2d914
Emir <eartar@sabanciuniv.edu>
2020-11-30 12:06:53 +0300

    Added JPA annotations to BaseEntity. closes #28.

--- pet-clinic-data/src/main/java/com/eartar/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/eartar/springpetclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.eartar.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass //this wont be in database, other classes will inherit it
 public class BaseEntity implements Serializable {
 
     //boxed types are always better since they can be nulled
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //identity -> simple auto incremented
     private Long id;
 
     public Long getId() {
