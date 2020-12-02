567d04ac238efb08d0ea89f74fd16997f6594589
Emir <eartar@sabanciuniv.edu>
2020-12-02 14:06:35 +0300

    Owners pets are displayed on /owners/{id}. closes #37.

--- pet-clinic-web/src/test/java/com/eartar/springpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/eartar/springpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -62,6 +63,18 @@ class OwnerControllerTest {
                 .andExpect(model().attribute("owners", hasSize(2)));
     }
 
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findByID(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
+
+
     @Test
     void findOwners() throws Exception {
 
@@ -71,4 +84,5 @@ class OwnerControllerTest {
 
         verifyZeroInteractions(ownerService);
     }
+
 }
